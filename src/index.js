06e1ce8890696a4bcd155691699c95288c2a9e35
johnshenk <jshenk@demandanlytics.io>
2025-06-06 07:26:28 -0500

    add snapshotify to prerender

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { hydrateRoot, createRoot } from 'react-dom/client';
+import loadScripts from 'snapshotify';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  // Page was pre-rendered — hydrate it
+  loadScripts().then(() => {
+    hydrateRoot(rootElement, <React.StrictMode><App /></React.StrictMode>);
+  });
+} else {
+  // No pre-rendered content — do a client-side render
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
